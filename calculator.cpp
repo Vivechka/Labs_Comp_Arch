86a68cd853b530710d9f793b04d63970e37395ac
Vivechka <58946742+Vivechka@users.noreply.github.com>
2021-10-17 22:31:47 +0300

    Update calculator.cpp

--- calculator.cpp
+++ calculator.cpp
@@ -1,6 +1,6 @@
 #include "calculator.h"
 //сomments are edition too:)
-//I can wrote some usefull stuff OR i can quote "Lord of the Rings"
+//I can write some usefull stuff OR i can quote "Lord of the Rings"
 //so
 //It's a dangerous business, Frodo, going out your door.
 // All we have to decide is what to do with the time that is given us.
